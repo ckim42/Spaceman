b624ed9342028e835d275dbd9c1f864949f073e0
ilikecoffee18 <42522882+ilikecoffee18@users.noreply.github.com>
2018-09-12 22:24:00 -0700

    Add files via upload
    Final version! Uploaded Sept. 12, 2018

--- Spacemangame.py
+++ Spacemangame.py
@@ -2,12 +2,25 @@ from random import random,randint
 
 wordsList = ["apple", "orange", "banana", "pineapple", "papaya", "tangerine", "kiwi", "cantelope", "watermelon", "honeydew", "grapefruit", "grape", "blueberry", "strawberry", "blackberry", "peach", "apricot", "cherry", "guava", "lemon", "lime", "mango", "lychee", "pear", "pomegranate" ]
 guessesList = []
+# guessesList = [item.lower() for item in guessesList] #WHAT
 chosenWord = wordsList[randint(0,len(wordsList)-1)]
 splitString = list(chosenWord) #splits string from wordsList into an array of its characters
 userInputString = ["_"]*len(splitString)
+userNumWrongGuesses = 0
+userGuessesRemaining = 7
 
 def getUserGuesses():
-    return input("This is a fruit. Guess a letter! ")
+    permittedStuff = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+    while True:
+        while True:
+            try:
+                letterGetter = input("This is a fruit. Guess a letter! ").lower()
+                break
+            except EOFError:
+                print("ERROR. ENTER A LETTER")
+        if (len(letterGetter) == 1 and letterGetter in permittedStuff):
+            return letterGetter #I know it's an extra step - it was a thing I think I needed to do because of the .lower()
+        print("ERROR. ENTER A LETTER")
 
 def displaySlashesEtc(placement):
     printString = ""
@@ -16,6 +29,7 @@ def displaySlashesEtc(placement):
         printString += " "
     print (printString)
     print ("guessed letters: " + ', '.join(guessesList)) #prints without the brackets!
+    print ("You have " + str(userGuessesRemaining) + " guesses left!")
 
 def replaceBlanks(split,uIn): #Here I'm trying to replace _ with correct guesses
     userGuess = getUserGuesses()
@@ -29,11 +43,11 @@ def replaceBlanks(split,uIn): #Here I'm trying to replace _ with correct guesses
 
 displaySlashesEtc(userInputString)
 userRight = True
-userNumWrongGuesses = 0
 while(not("_" not in userInputString or userNumWrongGuesses>=7)):
     userInputString,userRight = replaceBlanks(splitString, userInputString)
     if not userRight:
         userNumWrongGuesses+=1
+        userGuessesRemaining = 7 - userNumWrongGuesses
         print("WRONG!")
     displaySlashesEtc(userInputString)
 if(userNumWrongGuesses>=7):
